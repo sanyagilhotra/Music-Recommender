eb8bcfa931496f42a5a69d1ac77b41b7e84bf5c8
sanyagilhotra <gilhotrasanya@gmail.com>
2024-04-07 21:27:34 +0530

    Backend:Sentiment Analysis Added

--- frontend.py
+++ frontend.py
@@ -4,9 +4,4 @@ with open("song.txt", "r") as file:
 list_of_recs=get_selection(artist='Sabrina Carpenter',song='Thumbs',text="happy",rec_num=5)
 print("\nRecommended Songs:\n")
 for i in list_of_recs:
-    print(i[1]," by ",i[2],'\n')
-
-
-    
-    
-    
+    print(i[0]," by ",i[1][1],'\n')
--- recommender_script.py
+++ recommender_script.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from typing import List, Dict
+from textblob import TextBlob
 
 # %%
 songs = pd.read_csv('songdata (1).csv')
@@ -36,39 +37,47 @@ unique_artists=set(artists)
 len(unique_artists)
 
 # %%
+def sentiment_analysis(input_song_lyrics, recommended_songs):
+    input_sentiment = TextBlob(input_song_lyrics).sentiment.polarity
+    sentiment_scores = {}
+    
+    for song_data in recommended_songs:
+        song_lyrics = song_data[1]  # Assuming lyrics are in the second position
+        sentiment_score = TextBlob(song_lyrics).sentiment.polarity
+        sentiment_scores[song_data[1]] = abs(sentiment_score - input_sentiment),song_data[2]
+    
+    # Sort the songs based on sentiment difference
+    sorted_songs = sorted(sentiment_scores.items(), key=lambda x: x[1])
+
+    # Return top rec_num sentimentally similar songs
+    return sorted_songs
+
+# %%
 def get_selection(artist,song,text,rec_num):
+    
     global songs
+    
+    # add the song for recommendations to the dataset
     x={'artist':artist,'song':song,'text':text}
     x=pd.DataFrame([x])
     songs = songs.sample(n=3000)
     songs=pd.concat([songs,x])
+    
+    # finding the tfidf scores for all 
     tfidf = TfidfVectorizer(analyzer='word', stop_words='english')
     lyrics_matrix = tfidf.fit_transform(songs['text'])
+    
+    # finding the cosine similarities for the songs based on tf-idf scores
     cosine_similarities = cosine_similarity(lyrics_matrix) 
     similarities = {}
+    
+    # stores the top 50 cosime similarities in similarities dictionary 
     for i in range(len(cosine_similarities)):
         similar_indices = cosine_similarities[i].argsort()[:-50:-1] 
         similarities[songs['song'].iloc[i]] = [(cosine_similarities[i][x], songs['song'].iloc[x], songs['artist'].iloc[x]) for x in similar_indices][1:]
-    rec_data = {
-    "song": songs['song'].iloc[3000],
-    "number_songs": rec_num }
-    rec_list=similarities[song][1:rec_num+1]
     
-    
-    return rec_list
-
-# %%
-with open("song.txt", "r") as file:
-    song_lyrics = file.read().replace("\n", "")
-
-
-# %%
-list_of_recs=get_selection(artist='Sabrina Carpenter',song='Thumbs',text=song_lyrics,rec_num=5)
-
-# %%
-# print("\nRecommended Songs:\n")
-# for i in list_of_recs:
-#     print(i[1]," by ",i[2])
+    rec_list=sentiment_analysis(text,similarities[song][1:])
+    return rec_list[:5]
 
 # %%
 
